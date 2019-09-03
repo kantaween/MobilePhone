39ce5057e37b6694af2016e9d9803eb5fabd27e4
Kantawee Nithayothan <kantawee.nithayothan@scb.co.th>
2019-09-03 17:19:40 +0700

    set @Parcelize in model

--- app/src/main/java/com/codemobile/mobilephonebuyersguideapp/models/Mobile.kt
+++ app/src/main/java/com/codemobile/mobilephonebuyersguideapp/models/Mobile.kt
@@ -1,52 +1,32 @@
 package com.codemobile.mobilephonebuyersguideapp.models
 
-import android.os.Parcel
 import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Mobile(
+    @SerializedName("brand")
     val brand: String,
+
+    @SerializedName("description")
     val description: String,
+
+    @SerializedName("id")
     val id: Int,
+
+    @SerializedName("name")
     val name: String,
+
+    @SerializedName("price")
     val price: Double,
+
+    @SerializedName("rating")
     val rating: Double,
+
+    @SerializedName("thumbImageURL")
     val thumbImageURL: String,
+
     var favourite: Boolean = false
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString().toString(),
-        parcel.readString().toString(),
-        parcel.readInt(),
-        parcel.readString().toString(),
-        parcel.readDouble(),
-        parcel.readDouble(),
-        parcel.readString().toString(),
-        parcel.readByte() != 0.toByte()
-    )
 
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(brand)
-        parcel.writeString(description)
-        parcel.writeInt(id)
-        parcel.writeString(name)
-        parcel.writeDouble(price)
-        parcel.writeDouble(rating)
-        parcel.writeString(thumbImageURL)
-        parcel.writeByte(if (favourite) 1 else 0)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Mobile> {
-        override fun createFromParcel(parcel: Parcel): Mobile {
-            return Mobile(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Mobile?> {
-            return arrayOfNulls(size)
-        }
-    }
-
-}
+) : Parcelable
--- app/src/main/java/com/codemobile/mobilephonebuyersguideapp/models/MobileImage.kt
+++ app/src/main/java/com/codemobile/mobilephonebuyersguideapp/models/MobileImage.kt
@@ -1,7 +1,17 @@
 package com.codemobile.mobilephonebuyersguideapp.models
 
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class MobileImage(
+    @SerializedName("id")
     val id: Int,
+
+    @SerializedName("mobile_id")
     val mobile_id: Int,
+
+    @SerializedName("url")
     val url: String
-)
+): Parcelable
