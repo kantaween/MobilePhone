aefa37148daed347781f6df7f4680b0e9dee9eec
Kantawee Nithayothan <kantawee.nithayothan@scb.co.th>
2019-09-06 10:05:36 +0700

    Unit test get MobileList

--- app/src/test/java/com/codemobile/mobilephonebuyersguideapp/MobileInfoPresenterTest.kt
+++ app/src/test/java/com/codemobile/mobilephonebuyersguideapp/MobileInfoPresenterTest.kt
@@ -1,11 +1,11 @@
 package com.codemobile.mobilephonebuyersguideapp
 
-import android.util.Log
 import com.codemobile.mobilephonebuyersguideapp.interfaces.MainActivityPresenterInterface
 import com.codemobile.mobilephonebuyersguideapp.models.Mobile
 import com.codemobile.mobilephonebuyersguideapp.presenter.MainActivityPresenter
 import com.codemobile.mobilephonebuyersguideapp.service.MobileApiService
 import com.nhaarman.mockitokotlin2.*
+import mockit.Deencapsulation
 import org.junit.Before
 import org.junit.Test
 import org.mockito.ArgumentMatchers.anyString
@@ -31,6 +31,9 @@ class MobileInfoPresenterTest {
 
     @InjectMocks
     lateinit var presenter: MainActivityPresenter
+
+    private fun getMockMobile(id:Int = 1, price:Double = 100.0, rating: Double = 3.0, name: String = "any Name", favourite: Boolean = false) =
+        Mobile("any brand", "any description", id, name, price, rating, "any Url", favourite)
 
     @Before
     fun setup() {
@@ -81,4 +84,67 @@ class MobileInfoPresenterTest {
         //then
         verifyZeroInteractions(testView)
     }
+
+    @Test
+    fun testGetMobileListApiResponseBodyEmptyList() {
+        //given
+        val mockMobile1 = getMockMobile()
+        val mockFavouriteList = listOf(mockMobile1)
+        val call = mock<Call<List<Mobile>>>()
+        whenever(service.getMobileList()).thenReturn(call)
+        whenever(call.enqueue(any())).thenAnswer {
+            it.getArgument<Callback<List<Mobile>>>(0).onResponse(mock(), Response.success(listOf()))
+        }
+        Deencapsulation.setField(presenter, "mFavouriteList", mockFavouriteList)
+
+        //when
+        presenter.loadNewData()
+
+        //then
+        verifyZeroInteractions(testView)
+    }
+
+    @Test
+    fun testGetMobileListApiResponseBodyNoMatchIdWithFacouriteList() {
+        //given
+        val mockMobile1 = getMockMobile(id = 1)
+        val mockMobile2 = getMockMobile(id = 2)
+        val mockFavouriteList = listOf(mockMobile1)
+        val responseData = listOf(mockMobile2)
+        val call = mock<Call<List<Mobile>>>()
+        whenever(service.getMobileList()).thenReturn(call)
+        whenever(call.enqueue(any())).thenAnswer {
+            it.getArgument<Callback<List<Mobile>>>(0).onResponse(mock(), Response.success(responseData))
+        }
+        Deencapsulation.setField(presenter, "mFavouriteList", mockFavouriteList)
+
+        //when
+        presenter.loadNewData()
+
+        //then
+        verify(testView).updateData(responseData, mockFavouriteList)
+    }
+
+    @Test
+    fun testGetMobileListApiResponseBodyHaveMatchIdWithFavouriteList() {
+        //given
+        val mockMobile1 = getMockMobile(id = 1, favourite = true)
+        val mockMobile2 = getMockMobile(id = 1, favourite = false)
+        val mockFavouriteList = listOf(mockMobile1)
+        val responseData = listOf(mockMobile2)
+        val call = mock<Call<List<Mobile>>>()
+        whenever(service.getMobileList()).thenReturn(call)
+        whenever(call.enqueue(any())).thenAnswer {
+            it.getArgument<Callback<List<Mobile>>>(0).onResponse(mock(), Response.success(responseData))
+        }
+        Deencapsulation.setField(presenter, "mFavouriteList", mockFavouriteList)
+
+        //when
+        presenter.loadNewData()
+
+        //then
+        verify(testView).updateData(mockFavouriteList, mockFavouriteList)
+    }
+
+
 }
